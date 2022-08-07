c66f8f3d1f9e1418b6c4083b713518641ebad240
s-harada <satokotseso@gmail.com>
2022-08-07 06:09:53 +0000

    fix title color

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <pclass="top_title">NEWS</p>
+        <p class="top_title">NEWS</p>
         <hr color="#c0c0c0">
         @if (!is_null($headline))
             <div class="row">
